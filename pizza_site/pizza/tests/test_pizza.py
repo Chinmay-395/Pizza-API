37da686675d6e36212e4079b5f17b06221447298
Chinmay-395 <chinmaydali395@gmail.com>
2021-02-28 16:34:01 +0530

    The tests size test passed with OK and some warning for the djongo engine

--- pizza_site/pizza/tests/test_pizza.py
+++ pizza_site/pizza/tests/test_pizza.py
@@ -11,6 +11,12 @@ class SizeApiTests(TestCase):
     """
 
     def test_create_valid_size_success(self):
+        """Since we have create the Size(Pizza-Size) model with 
+            the dunder method of string(__str__).
+            It will return the string representation of the 
+            "name" attribute.
+            We will check if that is same as what we have set.
+        """
         size = Size.objects.create(name="Mediumish_Large")
 
         self.assertEquals(str(size), size.name)
